b19360deff2e2560642dcce2be17e4374858e039
avninf <93563175+avninf@users.noreply.github.com>
2022-03-28 11:38:47 +0700

    Update Products.vue

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -1,25 +1,22 @@
 <template>
+  <div>
+    <h1>Products</h1>
     <div class="row">
-      <div class="col-md-12">
-        <h1>Products</h1>
-        <div class="row">
-          <div class="col-md-4" v-for="produk in products" :key="produk.id">
-            <div class="card mb-4">
-              <div class="card-header">
-                <img :src="produk.foto" width="100%" class="img-thumb" />
-              </div>
-              <div class="card-body">
-                <h4>{{produk.nama}}</h4>
-                <h4>{{produk.harga}}</h4>
-                <!--tombol pesan-->
-                <a  v-if="produk.stok > 0" :href="produk.link_eksternal" target="_blank" class="btn btn-danger btn-block">beli</a>
-                <a v-else href="#" class="disables btn btn-dark btn-block">Dah habiss</a>
-              </div>
-            </div>
+      <div class="col-md-4" v-for="produk in products" :key="produk.id">
+        <div class="card mb-4">
+          <div class="card-header">
+            <img :src="produk.foto" width="100%" class="img-thumb" />
+          </div>
+          <div class="card-body">
+            <h4>{{ produk.nama }}</h4>
+            <h4>Rp{{ produk.harga }}</h4>
+            <a  v-if="produk.stok > 0" :href="produk.link_eksternal" target="_blank" class="btn btn-danger btn-block">beli</a>
+            <a v-else href="#" class="disable btn btn-dark btn-block">Stok Habis</a>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -30,13 +27,13 @@ export default {
     };
   },
   mounted() {
-    this.getProducts()
+    this.getProduk();
   },
   methods: {
-    async getProducts() {
-      let {data, error} = await this.$supabas.from("tb_produk").select().order('stok', {ascending: false});
-      if(data) this.products = data;
-      if(error) console.error(error);
+    async getProduk() {
+      let { data, error } = await this.$supabase.from("tb_produk").select().order('stok', {ascending: false});
+      if (data) this.products = data;
+      if (error) console.error(error);
     },
   },
 };
